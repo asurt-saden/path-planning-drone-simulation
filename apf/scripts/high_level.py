d5c45f8d2e6153154aef5520777f64ee691389c6
AlphaSnow10 <133535492+AlphaSnow10@users.noreply.github.com>
2024-05-14 20:17:08 +0300

    Add files via upload

---
+++ apf/scripts/high_level.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import math
+import rospy
+from geometry_msgs.msg import Quaternion, PoseStamped, Pose, Point
+from nav_msgs.msg import Path
+from std_msgs.msg import Bool, Float32
+from sensor_msgs.msg import Range
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import time
+import random
+
+def set_current_pose(data):
+    global current_point, current_orientation
+
+    current_point = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+
+    current_orientation = np.array([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+
+
+def obstacle_coordinates(drone_x, drone_y, drone_orientation, distance):
+    # Convert sensor direction to angle relative to world frame
+    angle = drone_orientation 
+    # Convert angle to radians
+    # Calculate obstacle coordinates relative to world frame
+    obstacle_x = drone_x + distance * np.cos(angle)
+    obstacle_y = drone_y + distance * np.sin(angle)
+    return obstacle_x, obstacle_y
+
+def set_obstacle_distance (data):
+    global obstacle_length
+    obstacle_length = data
+    
+    
+   
+def set_goal_pose(data):
+    global goal_point, goal_orientation
+
+    goal_point = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+
+    goal_orientation = np.array([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+
+def pi_2_pi(angle):
+    #A function to wrap the angle between -pi and pi (for numerical stability)
+    return (angle + np.pi) % (2 * np.pi) - np.pi
+
+def trig (x,y,x_c,y_c):
+    angle_rad = math.atan2(y - y_c, x - x_c)
+    #angle_deg = math.degrees(angle_rad)
+    if angle_rad < 0:
+        angle_rad += np.pi*2
+    return angle_rad
+
+
+def clock_wise (angle):
+    angle = angle + (np.pi/2)
+    angle = pi_2_pi(angle)
+    return angle
+
+def anti_clock_wise (angle):
+    angle = angle - (np.pi/2)
+    angle = pi_2_pi(angle)
+    return angle
+
+def one_eighty (angle):
+    angle = angle + np.pi
+    angle = pi_2_pi(angle)
+    return angle
+
+def set_point(x,y,z,x_th,y_th,z_th):
+
+    target_msg = PoseStamped()
+    target_msg.header.stamp = rospy.Time.now()
+    target_msg.pose.position.x = x
+    target_msg.pose.position.y = y
+    target_msg.pose.position.z = z  # Assuming z-coordinate is 0 for control point
+    target_msg.pose.orientation = Quaternion(*quaternion_from_euler(x_th, y_th, z_th))
+
+    target_pub.publish(target_msg)
+
+if __name__ == '__main__':
+    rospy.init_node("high_level")
+
+    sub_current_pose = rospy.Subscriber('/Pos', PoseStamped, set_current_pose)
+    sub_goal_pose = rospy.Subscriber('/Target_Pos', PoseStamped, set_goal_pose)
+    target_pub = rospy.Publisher('/Target_commands',PoseStamped, queue_size=10)
+    #obstacle_pub = rospy.Publisher('/obstacle_coord',PoseStamped,queue_size=10)
+    #obstacle_sub = rospy.Subscriber('/obstacle',Float32,set_obstacle_distance)
+
+    current_point = np.array([0.0, 0.0, 0.0])  # Initialize as numpy array
+    #obstacle_length = -1.0
+    #obstacle_coords = PoseStamped()
+
+
+    x = 0.0
+    y = 0.0
+    z = 1.0
+    x_c = 0.0
+    y_c = 0.0
+
+
+    step_size = 1.0
+
+    step_size_time = 10.0
+
+    #time.sleep(step_size_time)
+
+
+    while not rospy.is_shutdown():
+
+        x = float(random.randint(-5, 5)) 
+        y = float(random.randint(-5, 5))
+
+        z_th = trig(x,y,x_c,y_c)
+        
+        start_time = time.time()
+
+        while time.time() - start_time < 10:
+            # Your code here
+
+            set_point(x_c,y_c,z,np.pi/1000,np.pi/1000,z_th)
+
+            if rospy.is_shutdown():
+             break  # Exit the loop if rospy is shutdown
+
+            
+        start2_time = time.time()
+        while time.time() - start2_time < 20:
+
+            #obstacle_coords.pose.position.x, obstacle_coords.pose.position.y = obstacle_coordinates(current_point[0],current_point[1],z_th,obstacle_length)
+            #obstacle_pub.publish()
+            set_point(x, y, z, np.pi/1000, np.pi/1000, z_th)   
+            
+            if rospy.is_shutdown():
+             break
+
+        x_c = x
+        y_c = y
+    
+
+
+
+
+
+
---
+++ apf/scripts/my_apf.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+import numpy as np
+import rospy
+from geometry_msgs.msg import Quaternion, PoseStamped, Pose
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+def set_current_pose(data):
+    global current_point, current_orientation
+
+    current_point = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+
+    current_orientation = np.array([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+
+
+def set_goal_pose(data):
+    global goal_point, goal_orientation
+
+    goal_point = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+
+    goal_orientation = np.array([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+
+    goal_orientation = euler_from_quaternion(goal_orientation)
+
+
+class Dot:
+    def __init__(self, start, goal, gorientation, obstacles, k_att = 1, k_rep=100, rep_radius = 1, max_iter=10, threshold=0.1):
+        self.start = np.array(start)
+        self.goal = np.array(goal)
+        self.gorientation = np.array(gorientation)
+        self.obstacles = np.array(obstacles)
+        self.rep_radius = rep_radius
+        self.k_att = k_att
+        self.k_rep = k_rep
+        self.max_iter = max_iter
+        self.threshold = threshold
+        self.positions = [self.start.copy()]  # Initialize list to store positions
+
+    def attractive_potential(self, position):
+        return 0.5 * self.k_att * np.linalg.norm(position - self.goal)**2
+
+    def repulsive_potential(self, position):
+        rep_potential = 0
+        for obstacle in self.obstacles:
+            distance = np.linalg.norm(position - obstacle)
+            if distance < self.rep_radius:
+                rep_potential += 0.5 * self.k_rep * (1 / distance - 1 / self.rep_radius)**2
+        return rep_potential
+
+    def total_potential(self, position):
+        return self.attractive_potential(position) + self.repulsive_potential(position)
+
+    def gradient(self, position):
+        delta = 0.001
+        dx = (self.total_potential(position + np.array([delta, 0, 0])) - self.total_potential(position - np.array([delta, 0, 0]))) / (2 * delta)
+        dy = (self.total_potential(position + np.array([0, delta, 0])) - self.total_potential(position - np.array([0, delta, 0]))) / (2 * delta)
+        dz = (self.total_potential(position + np.array([0, 0, delta])) - self.total_potential(position - np.array([0, 0, delta]))) / (2 * delta)
+        return np.array([dx, dy, dz])
+
+
+    def move(self):
+        step_size = 0.1
+        max_iter = self.max_iter
+        threshold = self.threshold
+
+        for _ in range(max_iter):
+            gradient = self.gradient(self.start)
+
+            # Update position based on gradient descent
+            self.start -= step_size * gradient
+
+            # Create a PoseStamped message
+            control_msg = PoseStamped()
+            control_msg.header.stamp = rospy.Time.now()
+            control_msg.pose.position.x = self.start[0]
+            control_msg.pose.position.y = self.start[1]
+            control_msg.pose.position.z = 1.0
+
+
+            
+            control_msg.pose.orientation = Quaternion(*quaternion_from_euler(self.gorientation[0], self.gorientation[1], self.gorientation[2]))
+
+            # Publish the PoseStamped message
+            control_pub.publish(control_msg)
+
+                
+            # Append the new position to the list of positions
+            # self.positions.append(self.start.copy())
+
+if __name__ == '__main__':
+    rospy.init_node("apf")
+
+    sub_current_pose = rospy.Subscriber('/Pos', PoseStamped, set_current_pose)
+    sub_goal_pose = rospy.Subscriber('/Target_Pos', PoseStamped, set_goal_pose)
+    control_pub = rospy.Publisher('/Control_Pose', PoseStamped, queue_size=10)
+    #obstacles_sub = rospy.Subscriber('/obstacle_coord', PoseStamped, set_obstacle)
+
+
+    current_point = np.array([0.0, 0.0, 0.0])  # Initialize as numpy array
+    current_orientation = np.array([0.0, 0.0, 0.0, 0.0])
+    goal_point = np.array([0.0, 0.0, 0.0])  # Initialize as numpy array
+    goal_orientation = np.array([0.0, 0.0, 0.0, 0.0])
+
+    obstacles = np.array([[-3, -3, 1], [0, -2, 1],[-3, 3, 1],[4, 2, 1],[0, 2, 1],[4, -2, 1]])  # Convert to numpy array for easier calculations
+    #rep_radii = [1, 1.5]  # Radius for each obstacle
+
+    while not rospy.is_shutdown():
+        my_dot = Dot(current_point, goal_point, goal_orientation, obstacles)
+        my_dot.move()
+
